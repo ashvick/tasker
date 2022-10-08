485deebb0178337f15fb08d1f085c45b53b6cb46
Andrew <jellyvpr@gmail.com>
2022-10-08 16:57:13 +0400

    add bodyParser import

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 require('dotenv').config();
 
 app.use(cors()) // to allow cross origin requests
